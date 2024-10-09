7d0dddab837db0caa3c0132d773a1d44ea271675
Qiu Xia <qchu0005@student.monash.edu>
2024-10-09 20:40:26 +0800

    use old chatbot

--- Crop-Dashboard/views/chatbot.py
+++ Crop-Dashboard/views/chatbot.py
@@ -41,25 +41,21 @@ if 'uploaded_df' in st.session_state:
     # Initialize chat history
     if "messages" not in st.session_state:
         st.session_state.messages = []
-
+    
     # Display chat messages from history on app rerun
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
-    # React to user input
+     # React to user input
     if prompt := st.chat_input("What do you want to know about your dataset?"):
         # Display user message in chat message container
         st.chat_message("user").markdown(prompt)
+        # Add user message to chat history
         st.session_state.messages.append({"role": "user", "content": prompt})
 
-    #     # Generate response based on the prompt
-    #     response = get_openai_response(prompt)  # Get response from OpenAI API
-    #     st.chat_message("assistant").markdown(response)  # Display response in chat message container
-    #     # Add assistant response to chat history
-        response = ""
-        st.session_state.messages.append({"role": "assistant", "content": response})
         # Generate response based on the prompt
+        response = ""
 
 
         if "summary" in prompt.lower() or "summarize" in prompt.lower() or "summarise" in prompt.lower():
